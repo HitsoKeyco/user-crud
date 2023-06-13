0c4d397e701192954d58c57884a2f3171afc07b1
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-06-13 18:16:39 -0500

    usercard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import useFetch from './hooks/useFetch'
-import FormUsers from './components/FormUsers'
-import UserCard from './components/Usercard'
+import FormUsers from '../components/FormUsers'
+import UserCard from '../components/Usercard'
 
 function App() {
   
